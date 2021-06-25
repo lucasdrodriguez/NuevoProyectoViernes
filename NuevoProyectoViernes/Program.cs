b6e21da9ad174b14f8fd246d76f34db585b13518
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2021-06-25 20:17:34 -0300

    Initial commit

---
+++ NuevoProyectoViernes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NuevoProyectoViernes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola pepeee!");
+        }
+    }
+}
