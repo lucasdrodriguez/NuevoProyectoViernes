e3925988aa384b3479d25b7783aec44807e89c1f
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2021-06-25 21:02:39 -0300

    version 1.1 apellidos

--- NuevoProyectoViernes/Program.cs
+++ NuevoProyectoViernes/Program.cs
@@ -9,9 +9,15 @@ namespace NuevoProyectoViernes
             Console.WriteLine("Ingrese nombre");
             string nombre = Console.ReadLine();
 
+
+            Console.WriteLine("Ingrese apellido");
+            string apellido = Console.ReadLine();
+            
+
             Console.Clear();
 
             Console.WriteLine(nombre);
+            Console.WriteLine(apellido);
 
         }
     }
